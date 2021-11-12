fa6f52552bdfafda8a3de0a1aa50d8ac18b7cab4
Clive B Harrison <clive_b_harrison@outlook.com>
2021-11-12 22:03:57 +0000

    Add project files.

---
+++ Variables CBH/Form1.vb
@@ -0,0 +1,23 @@
+﻿Public Class Form1
+
+    ''' <summary>
+    ''' 
+    ''' </summary>
+    ''' <param name="sender"></param>
+    ''' <param name="e"></param>
+
+    Private Sub btnAdd_Click(sender As Object, e As EventArgs) Handles btnAdd.Click
+        'Define a variable for intNumber
+        Dim intNumber As Integer
+
+        'Set the initial value
+        intNumber = 27
+
+        'Add 1 to the value of intNumber
+        intNumber = intNumber + 1
+
+        'Display the new value of intNumber
+        MessageBox.Show("Value of intNumber + 1 = " & intNumber.ToString,
+        "Variables")
+    End Sub
+End Class
---
+++ Variables/Form1.vb
@@ -0,0 +1,17 @@
+﻿Public Class Form1
+
+    ''' <summary>
+    ''' 
+    ''' </summary>
+    ''' <param name="sender"></param>
+    ''' <param name="e"></param>
+    ''' <remarks></remarks>
+    Private Sub btnAdd_Click(ByVal sender As System.Object, _
+        ByVal e As System.EventArgs) Handles btnAdd.Click
+        Dim intNumber As Integer
+        intNumber = 27
+        intNumber = intNumber + 1
+        MessageBox.Show("Value of intNumber + 1 = " & intNumber.ToString, _
+           "Variables")
+    End Sub
+End Class
